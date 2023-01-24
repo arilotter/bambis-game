3fb1a338f1d9fb60c263e386818cd4b6a30bee05
Ari Lotter <arilotter@gmail.com>
2023-01-24 17:01:32 -0500

    fix bullet-bullet collisions

--- client/src/collision_groups.rs
+++ client/src/collision_groups.rs
@@ -4,5 +4,6 @@ pub const COL_DUDE: Group = Group::GROUP_1;
 pub const COL_BULLET: Group = Group::GROUP_2;
 pub const COL_TERRAIN: Group = Group::GROUP_3;
 
-pub const COL_FILTER_BULLET: Group = COL_TERRAIN;
+pub const COL_FILTER_BULLET: Group =
+    Group::from_bits_truncate(COL_TERRAIN.bits() | COL_BULLET.bits());
 pub const COL_FILTER_DUDE: Group = COL_TERRAIN;
